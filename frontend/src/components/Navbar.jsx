d2574b4606576debea93c44590bde88615a45f45
PharesMwangi <mwasmwas683@gmail.com>
2026-08-26 15:09:36 +0300

    added missing () to useNavigate

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function Navbar(){
-    const { user, logout } = useAuth;
-    const navigate = useNavigate;
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
 
     const handleLogout = async () => {
         await logout();
