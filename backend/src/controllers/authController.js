2d758d0df4a11e3c62b91547f9f13ad010f345b0
PharesMwangi <mwasmwas683@gmail.com>
2026-08-27 12:23:54 +0300

    changed cookie options

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,8 +3,8 @@ const User = require("../models/User");
 
 const COOKIE_OPTIONS ={
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
